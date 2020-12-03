6b27fe77353cceb0a0091d51b4fc8f35f00164c2
nikodem911 <nikodem.jura@gmail.com>
2020-12-03 19:47:25 +0100

    Initial commit - app generated from Spring Initializr

---
+++ demo-app/src/main/kotlin/org/demo/demoapp/DemoAppApplication.kt
@@ -0,0 +1,11 @@
+package org.demo.demoapp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoAppApplication
+
+fun main(args: Array<String>) {
+	runApplication<DemoAppApplication>(*args)
+}
